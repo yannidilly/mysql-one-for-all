8adb2f87431b07e852abc3df220511aab9c51add
Yanni <yannidilly.prog@gmail.com>
2023-01-30 10:08:19 -0300

    adiciona soma de tempo consumindo as musicas (finaliza o req2)

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,10 @@
 SELECT
 	user.user_name AS usuario,
-	COUNT(history.music_id) AS qt_de_musicas_ouvidas
+	COUNT(history.music_id) AS qt_de_musicas_ouvidas,
+    ROUND(SUM(music.duration) / 60, 2) AS total_minutos
 FROM SpotifyClone.history AS history
 INNER JOIN SpotifyClone.user AS user
 	ON history.user_id = user.user_id
-GROUP BY user.user_name;
+INNER JOIN SpotifyClone.music AS music
+	ON history.music_id = music.music_id
+GROUP BY user.user_name ORDER BY user.user_name;
