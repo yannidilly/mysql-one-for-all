145904aa680546c83c20b844b1a2bdadd74e3769
Yanni <yannidilly.prog@gmail.com>
2023-01-30 12:14:23 -0300

    agrupa resultados de atividade de cada usuário para eliminar repetições (fix req4)

--- desafio4.sql
+++ desafio4.sql
@@ -1,9 +1,11 @@
 SELECT 
     user.user_name AS usuario,
-    IF(history.played_date >= '2021-01-01 00:00:00.000001',
+    IF(MAX(history.played_date) >= '2021-01-01 00:00:00.000001',
         'Usuário ativo',
         'Usuário inativo') AS status_usuario
 FROM
     SpotifyClone.history AS history
         INNER JOIN
-    SpotifyClone.user AS user ON history.user_id = user.user_id;
+    SpotifyClone.user AS user ON history.user_id = user.user_id
+GROUP BY user.user_name
+ORDER BY user.user_name;
