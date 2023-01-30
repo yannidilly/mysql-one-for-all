f104d125bc1d5da4556584fc0a14929e30037cea
Yanni <yannidilly.prog@gmail.com>
2023-01-30 09:33:49 -0300

    requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,5 @@
+SELECT
+COUNT(DISTINCT music.music_id) AS cancoes,
+COUNT(DISTINCT artist.artist_id) AS artistas,
+COUNT(DISTINCT album.album_id) AS albuns
+FROM SpotifyClone.music, SpotifyClone.artist, SpotifyClone.album;
