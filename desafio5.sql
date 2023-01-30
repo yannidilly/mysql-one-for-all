43ab4b999208fc99fc4336227e2466bb50ee26c8
Yanni <yannidilly.prog@gmail.com>
2023-01-30 10:57:38 -0300

    cria query que exibe as duas músicas mais tocadas (req5)

---
+++ desafio5.sql
@@ -0,0 +1,10 @@
+SELECT 
+    music.music_name AS cancao,
+    COUNT(history.music_id) AS reproducoes
+FROM
+    SpotifyClone.history AS history
+        INNER JOIN
+    SpotifyClone.music AS music ON history.music_id = music.music_id
+GROUP BY history.music_id
+ORDER BY reproducoes DESC
+LIMIT 2;
