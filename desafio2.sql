62fd72c7264898291a722938cf74c06a9c022745
Yanni <yannidilly.prog@gmail.com>
2023-01-30 10:44:45 -0300

    melhora padrão de formatação do arquivo

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,8 @@
-SELECT
-COUNT(DISTINCT music.music_id) AS cancoes,
-COUNT(DISTINCT artist.artist_id) AS artistas,
-COUNT(DISTINCT album.album_id) AS albuns
-FROM SpotifyClone.music, SpotifyClone.artist, SpotifyClone.album;
+SELECT 
+    COUNT(DISTINCT music.music_id) AS cancoes,
+    COUNT(DISTINCT artist.artist_id) AS artistas,
+    COUNT(DISTINCT album.album_id) AS albuns
+FROM
+    SpotifyClone.music,
+    SpotifyClone.artist,
+    SpotifyClone.album;
--- desafio3.sql
+++ desafio3.sql
@@ -1,10 +1,12 @@
-SELECT
-	user.user_name AS usuario,
-	COUNT(history.music_id) AS qt_de_musicas_ouvidas,
+SELECT 
+    user.user_name AS usuario,
+    COUNT(history.music_id) AS qt_de_musicas_ouvidas,
     ROUND(SUM(music.duration) / 60, 2) AS total_minutos
-FROM SpotifyClone.history AS history
-INNER JOIN SpotifyClone.user AS user
-	ON history.user_id = user.user_id
-INNER JOIN SpotifyClone.music AS music
-	ON history.music_id = music.music_id
-GROUP BY user.user_name ORDER BY user.user_name;
+FROM
+    SpotifyClone.history AS history
+        INNER JOIN
+    SpotifyClone.user AS user ON history.user_id = user.user_id
+        INNER JOIN
+    SpotifyClone.music AS music ON history.music_id = music.music_id
+GROUP BY user.user_name
+ORDER BY user.user_name;
