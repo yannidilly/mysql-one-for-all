ea8fbd2a6caf0bca32935edc305560282730fcec
Yanni <yannidilly.prog@gmail.com>
2023-01-30 22:32:43 -0300

    adiciona contagem de seguires na query (req7)

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,12 @@
 SELECT 
-    artist.artist_name, album.album_name
+    artist.artist_name AS artista,
+    album.album_name AS album,
+    COUNT(follow.user_id) AS seguidores
 FROM
     SpotifyClone.artist AS artist
         INNER JOIN
-    SpotifyClone.album AS album ON artist.artist_id = album.artist_id;
+    SpotifyClone.album AS album ON artist.artist_id = album.artist_id
+        INNER JOIN
+    SpotifyClone.follow AS follow ON follow.artist_id = artist.artist_id
+GROUP BY album , artista
+ORDER BY seguidores DESC;
